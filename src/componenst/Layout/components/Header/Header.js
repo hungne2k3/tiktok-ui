10096d75f36a635bb01b4e1fa6430ab9ebd6a3a4
hungne2k3 <104420895+hungne2k3@users.noreply.github.com>
2023-09-11 00:40:12 -0700

    make UI Header or Search

--- src/componenst/Layout/components/Header/Header.js
+++ src/componenst/Layout/components/Header/Header.js
@@ -1,6 +1,12 @@
+// import thu vien icon
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import ten icon
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 // thu vien className: npm i classnames;
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '../../../../assest/images';
 
 const cx = classNames.bind(styles);
 
@@ -8,8 +14,25 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                {/* logo */}
-                {/* search */}
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="TikTok" />
+                </div>
+
+                <div className={cx('sreach')}>
+                    <input placeholder="Sreach accounts and video" spellCheck={false} />
+
+                    <button className={cx('clean')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                    <button className={cx('sreach-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+
+                <div className={cx('action')}></div>
             </div>
         </header>
     );
